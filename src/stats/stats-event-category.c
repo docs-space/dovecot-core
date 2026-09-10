6ed617c0cd8b840e6a6bb3b0826e516d4844a38f
Timo Sirainen <timo.sirainen@open-xchange.com>
2026-09-10 07:45:55 +0000

    stats: Unregister event categories before freeing them
    The categories are allocated from a pool that is freed at deinit, while
    lib-event kept pointers to them. In fuzzing builds this caused a write to
    freed memory in lib_event_deinit(), which was visible e.g. as a valgrind
    error in test-stats-metrics.

--- src/stats/stats-event-category.c
+++ src/stats/stats-event-category.c
@@ -1,9 +1,11 @@
 /* Copyright (c) Dovecot authors, see top-level COPYING file */
 
 #include "lib.h"
+#include "array.h"
 #include "stats-event-category.h"
 
 static pool_t categories_pool;
+static ARRAY(struct event_category *) registered_categories;
 
 void stats_event_category_register(const char *name,
 				   struct event_category *parent)
@@ -12,6 +14,7 @@ void stats_event_category_register(const char *name,
 		p_new(categories_pool, struct event_category, 1);
 	category->parent = parent;
 	category->name = p_strdup(categories_pool, name);
+	array_push_back(&registered_categories, &category);
 
 	/* Create a temporary event to register the category. A bit slower
 	   than necessary, but this code won't be called often. */
@@ -24,9 +27,16 @@ void stats_event_category_register(const char *name,
 void stats_event_categories_init(void)
 {
 	categories_pool = pool_alloconly_create("categories", 1024);
+	p_array_init(&registered_categories, categories_pool, 16);
 }
 
 void stats_event_categories_deinit(void)
 {
+	struct event_category *category;
+
+	/* The categories stay registered in lib-event, but the structs
+	   allocated from the pool must not be referenced anymore. */
+	array_foreach_elem(&registered_categories, category)
+		event_category_unregister(category);
 	pool_unref(&categories_pool);
 }
